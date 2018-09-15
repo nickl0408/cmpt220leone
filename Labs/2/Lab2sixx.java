52266d4b233dd70f61190275ec93c67579230b4d
Nick Leone <nicholas.leone1@marist.edu>
2018-09-14 21:42:23 -0400

    lab2
    ? 6

---
+++ Labs/2/Lab2sixx.java
@@ -0,0 +1,22 @@
+
+public class Lab2sixx {
+
+	public static void main(String[] args) {
+		
+		int num1 = (int)(Math.random() * (8)) + 2;
+		int num2 = (int)(Math.random() * 10);
+		int num3 = (int)(Math.random() * 10);
+		int num4 = (int)(Math.random() * 10);
+		int num5 = (int)(Math.random() * 10);
+		int num6 = (int)(Math.random() * 10);
+		int num7 = (int)(Math.random() * 10);
+		int num8 = (int)(Math.random() * 10);
+		int num9 = (int)(Math.random() * 10);
+		int num10 = (int)(Math.random() * 10);
+		
+		System.out.print(" Random Phone number = (" + num1 + num2 + num3 +")-" 
+		+ num4 + num5 + num6 + "-" + num7 + num8 + num9 + num10);
+
+	}
+
+}
