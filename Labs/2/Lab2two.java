07777c78436e439edc79e9134e03bbbb555251f8
Juan Arias <juan.arias@marist.edu>
2018-09-18 21:04:05 -0400

    Feedback

--- Labs/2/Lab2two.java
+++ Labs/2/Lab2two.java
@@ -8,49 +8,27 @@ public class Lab2two {
 		double month = input.nextDouble();
 		if (month == 1)
 			System.out.print("January");
-		else
-		
-		if (month == 2)
+		else if (month == 2)
 				System.out.print("February");
-		else
-		
-		if (month == 3)
+		else if (month == 3)
 				System.out.print("March");
-		else
-			
-		if (month == 4)
+		else if (month == 4)
 				System.out.print("April");
-		else
-		
-		if (month == 5)
+		else if (month == 5)
 				System.out.print("May");
-		else
-
-		if (month == 6)
+		else if (month == 6)
 				System.out.print("June");
-		else
-			
-		if (month == 7)
+		else if (month == 7)
 				System.out.print("July");
-		else
-			
-		if (month == 8)
+		else if (month == 8)
 				System.out.print("August");
-		else
-				
-		if (month == 9)
+		else if (month == 9)
 				System.out.print("September");
-		else
-			
-		if (month == 10)
+		else if (month == 10)
 				System.out.print("October");
-		else
-			
-		if (month == 11)
+		else if (month == 11)
 				System.out.print("November");
-		else
-		
-		if (month == 12)
+		else if (month == 12)
 				System.out.print("December");
 		else
 			System.out.print("That is an incorrect number");
