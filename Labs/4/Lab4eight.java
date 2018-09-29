957e368bcf2253ec09cbfc503d14877a06d7e1fc
Nick Leone <nicholas.leone1@marist.edu>
2018-09-28 23:42:16 -0400

    Lab4
    4

---
+++ Labs/4/Lab4eight.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class Lab4eight 
+{
+    public static void main(String[] args) 
+    {
+        int k, temp;
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter number of elements you want in array. You can do a maximum of 100:");
+        k = s.nextInt();
+        int a[] = new int[k];
+        System.out.println("Enter the numbers:");
+        for (int i = 0; i < k; i++) 
+        {
+            a[i] = s.nextInt();
+        }
+        for (int i = 0; i < k; i++) 
+        {
+            for (int j = i + 1; j < k; j++) 
+            {
+                if (a[i] > a[j]) 
+                {
+                    temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+                }
+            }
+        }
+        System.out.print("Ascending Order:");
+        for (int i = 0; i < n - 1; i++) 
+        {
+            System.out.print(a[i] + ",");
+        }
+        System.out.print(a[n - 1]);
+    }
+}
